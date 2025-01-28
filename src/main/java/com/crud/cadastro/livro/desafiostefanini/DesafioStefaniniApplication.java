ce43986bfa562854d1af78e7fcf1f3f229534f82
joao horacio <joaolago35@gmail.com>
2025-01-28 15:41:32 -0300

    configuraçao iniciais na master

---
+++ src/main/java/com/crud/cadastro/livro/desafiostefanini/DesafioStefaniniApplication.java
@@ -0,0 +1,13 @@
+package com.crud.cadastro.livro.desafiostefanini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioStefaniniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioStefaniniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crud/cadastro/livro/desafiostefanini/DesafioStefaniniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.cadastro.livro.desafiostefanini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioStefaniniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
